432e8de3b5b70459d7c48d32b6795c3193154587
Abir Mahmud <amabirmahmud@gmail.com>
2021-06-14 23:22:37 +0600

    monaco-editor implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MonacoEditorComponent} from "./monaco-editor/monaco-editor.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonacoEditorComponent
   ],
   imports: [
     BrowserModule
